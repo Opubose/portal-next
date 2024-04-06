ac37d1b90cf3cb919344983a95f38f7371ff60d1
Ruben Olano <rubendavid2711@gmail.com>
2024-04-06 13:10:56 -0500

    Update checkin component

--- pages/checkin/[slug].tsx
+++ pages/checkin/[slug].tsx
@@ -2,19 +2,24 @@ import { NextRouter, useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import LoadingComponent from 'components/LoadingComponent';
 import ErrorComponent from 'components/ErrorComponent';
-import { default as ACMButton } from '../../components/PortalButton';
 import SuccessfulComponent from 'components/SuccessfulComponent';
 import { useSession } from 'next-auth/react';
 import { gqlQueries } from 'src/api';
-import { useQuery } from 'react-query';
+import { useQuery } from '@tanstack/react-query';
 import { GraphQLError } from 'graphql';
 import Loading from 'components/Loading';
+import Link from 'next/link';
 
-function ViewWrapper({ children, router }: React.PropsWithChildren<{ router: NextRouter }>) {
+function ViewWrapper({ children }: React.PropsWithChildren) {
   return (
     <div className="flex flex-col gap-y-3">
       {children}
-      <ACMButton onClick={() => router.push('/events')}>click here to return to events</ACMButton>
+      <Link
+        className="bg-none dark:text-white text-black text-2xl hover:scale-105 transition-all ease-in-out hover:shadow-[inset_13rem_0_0_0] hover:shadow-violet-400 duration-[300ms,600ms] w-52 py-2"
+        href="/events"
+      >
+        click here to return to events
+      </Link>
     </div>
   );
 }
@@ -22,14 +27,15 @@ function ViewWrapper({ children, router }: React.PropsWithChildren<{ router: Nex
 export default function CheckinPage() {
   const router = useRouter();
   const { slug } = router.query;
-  const { status } = useSession({
-    required: true,
+  const { status } = useSession({ required: true });
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['checkInData'],
+    queryFn: async () => {
+      return await gqlQueries.getCheckInPageUserInfo();
+    },
+    enabled: status === 'authenticated',
   });
-  const { data, error, isLoading } = useQuery(
-    ['checkInData'],
-    () => gqlQueries.getCheckInPageUserInfo(),
-    { enabled: status === 'authenticated' },
-  );
 
   const [queryError, setQueryError] = useState<GraphQLError | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
@@ -66,7 +72,7 @@ export default function CheckinPage() {
   }
   if (queryError) {
     return (
-      <ViewWrapper router={router}>
+      <ViewWrapper>
         <ErrorComponent
           errorCode={queryError.extensions.code as string}
           errorMessage={queryError.message}
@@ -76,7 +82,7 @@ export default function CheckinPage() {
   }
 
   return (
-    <ViewWrapper router={router}>
+    <ViewWrapper>
       <SuccessfulComponent message="Check-in successful" />
     </ViewWrapper>
   );
