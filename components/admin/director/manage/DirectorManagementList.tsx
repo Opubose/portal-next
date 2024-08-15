1feffb49366e42bbeec9971077d99a9d7c8994bd
DedsecKnight <mikenguyen7268@gmail.com>
2024-08-15 14:49:52 -0500

    feat: added fuzzy search into director management view

--- components/admin/director/manage/DirectorManagementList.tsx
+++ components/admin/director/manage/DirectorManagementList.tsx
@@ -2,6 +2,10 @@ import { GetDirectorManagementPageInfoQuery } from 'lib/generated/graphql';
 import { Table, TableHeader, TableHead, TableBody, TableRow, TableCell } from 'components/ui/table';
 import { ScrollArea } from 'components/ui/scroll-area';
 import DirectorRemovalDialog from './DirectorRemovalDialog';
+import { useEffect, useMemo, useState } from 'react';
+import debounce from 'lodash.debounce';
+import Fuse from 'fuse.js';
+import { Input } from 'components/ui/input';
 
 interface DirectorManagementListProps {
   directors: GetDirectorManagementPageInfoQuery['directors'];
@@ -14,8 +18,37 @@ export default function DirectorManagementList({
   directors,
   handleDirectorRemoval,
 }: DirectorManagementListProps) {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredDirectors, setFilteredDirectors] =
+    useState<GetDirectorManagementPageInfoQuery['directors']>(directors);
+  const debounceSearchQuery = useMemo(() => {
+    return debounce((e) => setSearchQuery(e.target.value), 300);
+  }, []);
+  useEffect(() => {
+    if (searchQuery === '') {
+      setFilteredDirectors(directors);
+    } else {
+      const fuse = new Fuse(directors, {
+        keys: ['officer.profile.firstName', 'officer.profile.lastName'],
+      });
+      const result = fuse.search(searchQuery);
+      setFilteredDirectors(result.map((data) => data.item));
+    }
+  }, [searchQuery]);
+  useEffect(() => {
+    return () => {
+      debounceSearchQuery.cancel();
+    };
+  });
   return (
     <ScrollArea className="h-[700px] rounded-md border text-white p-5">
+      <div className="p-4">
+        <Input
+          className="rounded-xl p-3 text-black"
+          placeholder="Search director"
+          onChange={debounceSearchQuery}
+        />
+      </div>
       <Table>
         <TableHeader>
           <TableRow>
@@ -25,7 +58,7 @@ export default function DirectorManagementList({
           </TableRow>
         </TableHeader>
         <TableBody>
-          {directors.map((director) => (
+          {filteredDirectors.map((director) => (
             <TableRow key={director.id}>
               <TableCell>
                 {director.officer!.profile.firstName} {director.officer!.profile.lastName}
